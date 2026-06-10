db69a9a69ecbaad5533af65d7a46b65bf2621fe0
jashwanth5102495 <jashwanth5102495@users.noreply.github.com>
2026-06-10 22:48:39 +0530

    Initial commit

---
+++ src/productsData.js
@@ -0,0 +1,480 @@
+export const PRODUCTS = [
+  { 
+    brand: 'GREEN MIRACLE', 
+    specification: 'Kappaphycus alvarezii 19.5% (liquid)', 
+    category: 'Seaweed Extract',
+    crops: ['Apple', 'Tomato', 'Cucumber', 'Chilli'], 
+    dosage: [
+      'Apple Two foliar applications at 750 ml/ha',
+      'Tomato Three foliar applications at 500 ml/ha',
+      'Cucumber Three foliar applications at 500 ml/ha',
+      'Chilli Three foliar applications at 500 ml/ha'
+    ], 
+    gazette: '13th August, 2025 S.O. 3741(E)', 
+    composition: [
+      '(i) Content Seaweed (Kappaphycus alvarezii) extract) per cent. by volume, minimum 19.5', 
+      '(ii) Organic acid (acetic acid, citric acid and lactic acid) per cent. by volume, minimum 1.5',
+      '(iii) Water per cent. by volume, maximum 79',
+      '(iv) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'COTTON SUPIRIOR', 
+    specification: 'Ascophyllum nodosum 22% (liquid)', 
+    category: 'Seaweed Extract',
+    crops: ['Cotton', 'Soybean', 'Tomato'], 
+    dosage: [
+      'Cotton: Three foliar applications at 625 ml/ha',
+      'Soybean: Two foliar applications at 625 ml/ha',
+      'Tomato: Three foliar applications at 625 ml/ha'
+    ], 
+    gazette: '13th August, 2025 S.O. 3741(E)', 
+    composition: [
+      '(i) Seaweed (Ascophyllum nodosum) extract per cent. by weight, minimum 22%', 
+      '(ii) Water per cent. by weight QS',
+      '(iii) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'ECO SMART', 
+    specification: 'Seaweed (Sargassum wightii) extract 35 % (Powder)', 
+    category: 'Seaweed Extract',
+    crops: ['Chilli'], 
+    dosage: ['Four foliar applications at 250 g/ha'], 
+    gazette: '16th February, 2026 S.O. 876(E)', 
+    composition: [
+      '(i) Seaweed (Sargassum wightii) extract per cent. by weight, minimum 35.0', 
+      '(ii) Dextrin per cent. by weight, maximum QS',
+      '(iii) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'FENTASTIC', 
+    specification: 'Ascophyllum nodosum 20% (liquid)', 
+    category: 'Seaweed Extract',
+    crops: ['Chilli', 'Rice', 'Cotton', 'Bengal gram', 'Tomato', 'Soybean', 'Wheat'], 
+    dosage: [
+      'Chilli Four foliar applications at 625 ml/ha',
+      'Rice One soil application at 750 ml/ha',
+      'Cotton Two foliar applications at 750 ml/ha',
+      'Bengal gram Three foliar applications at 750 ml',
+      'Tomato Three foliar applications at 1000 ml/ha',
+      'Soybean Two foliar applications at 750 ml/ha',
+      'Wheat Two foliar applications at 1000 ml/ha'
+    ], 
+    gazette: '16th February, 2026 S.O. 876(E)', 
+    composition: [
+      '(i) Seaweed (Ascophyllum nodosum) extract per cent. by weight, minimum 20', 
+      '(ii) Sodium salt of Propyl Paraben per cent. by weight, minimum 0.25',
+      '(iii) Water per cent. by weight, maximum 79.75',
+      '(iv) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'SUPER SOIL', 
+    specification: 'Plant (Adhatoda vassica) extract 7% (Granules)', 
+    category: 'Botanical Extract',
+    crops: ['Potato'], 
+    dosage: ['One soil application at 10 kg/ha'], 
+    gazette: '16th February, 2026 S.O. 876(E)', 
+    composition: [
+      '(i) Plant (Adhatoda vassica) extract (equivalent to Vasicine 5 ppm) per cent. by weight, minimum 7.0', 
+      '(ii) Seaweed (Ascophyllum nodosum) extract per cent. by weight, minimum 5.0',
+      '(iii) Sesame oil per cent. by weight, minimum 0.1',
+      '(iv) Turmeric powder as carrier/filler per cent. by weight, maximum 0.1',
+      '(v) Sand per cent. by weight QS',
+      '(vi) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'GREEN WONDER', 
+    specification: 'Spirulina 10% (Liquid)', 
+    category: 'Botanical Extract',
+    crops: ['Chilli', 'Tomato'], 
+    dosage: ['Two foliar sprays at 750 ml/ha'], 
+    gazette: '26th May, 2025 S.O. 2346(E)', 
+    composition: [
+      '(i) Spirulina powder per cent. by weight, minimum 10', 
+      '(ii) Propylene glycol per cent. by weight, minimum 5',
+      '(iii) Lignin sulfonate per cent. by weight, minimum 5',
+      '(iv) Polysaccharide per cent. by weight, minimum 0.1',
+      '(v) Citric acid per cent. by weight, minimum 0.1',
+      '(vi) Water per cent. by weight QS',
+      '(vii) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'VASICA', 
+    specification: 'Adhatoda vasica extract 2% (Liquid)', 
+    category: 'Botanical Extract',
+    crops: ['Chilli', 'Tomato'], 
+    dosage: [], 
+    gazette: '26th May, 2025 S.O. 2346(E)', 
+    composition: [
+      '(i) Adhatoda vasica (leaf) extract per cent. by weight, minimum 2', 
+      '(ii) N propanol as solvent per cent. weight by volume, maximum 20',
+      '(iii) Vegetable (soybean) oil per cent. weight by volume, maximum 78',
+      '(iv) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'ENTER', 
+    specification: 'Basil oil (Eugenol) 2% (Liquid)', 
+    category: 'Botanical Extract',
+    crops: ['Tomato'], 
+    dosage: ['Three foliar applications at 1000 ml/ha'], 
+    gazette: '13th August, 2025 S.O. 3741(E)', 
+    composition: [
+      '(i) Basil oil (Eugenol) per cent. weight by volume, minimum 5', 
+      '(ii) Tween 80 per cent. weight by volume, minimum 6',
+      '(iii) Cotton seed oil per cent. weight by volume, minimum 89',
+      '(iv) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'MANTRA', 
+    specification: 'Botanical (Moringa oleifera) extract 13.5% (liquid)', 
+    category: 'Botanical Extract',
+    crops: ['Tomato'], 
+    dosage: ['Three soil applications at 1000 ml/ha'], 
+    gazette: '16th February, 2026 S.O. 876(E)', 
+    composition: [
+      '(i) Botanical (Moringa oleifera) extract per cent. by weight, minimum 13.5', 
+      '(ii) Citric acid per cent. by weight, minimum 0.5',
+      '(iii) Water per cent. by weight, maximum 86',
+      '(iv) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'MARVELL', 
+    specification: 'Botanical (Garlic) extract 1.6% (Liquid)', 
+    category: 'Botanical Extract',
+    crops: ['Tomato'], 
+    dosage: ['Three foliar applications at 1000 ml/ha'], 
+    gazette: '16th February, 2026 S.O. 876(E)', 
+    composition: [
+      '(i) Garlic oil per cent. by weight, minimum 1.6', 
+      '(ii) Tween 80 per cent. by weight, maximum 6.0',
+      '(iii) Cottonseed oil per cent. by weight, maximum 92.4',
+      '(iv) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'SUPER PADDY', 
+    specification: 'Mixture of Protein Hydrolysate and Seaweed extract (Liquid)', 
+    category: 'Mixed Formulations',
+    crops: ['Paddy', 'Tomato'], 
+    dosage: [
+      'Paddy Two foliar sprays at 1000 ml/ha',
+      'Tomato Two foliar sprays at 1000 ml/ha'
+    ], 
+    gazette: '', 
+    composition: [
+      '(i) Protein hydrolysate derived from Sus domesticus (animal source) through enzymatic (protease) hydrolysis per cent. by weight, minimum 52.98', 
+      '(ii) Seaweed (Ascophyllum nodosum) extract per cent. by weight, minimum 10',
+      '(iii) Propyl paraben per cent. by weight, minimum 0.34',
+      '(iv) Water per cent. by weight 36.68',
+      '(v) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'POTATAO POWER', 
+    specification: 'Mixture of Seaweed extract, Protein Hydrolysate and Humic acid (Granule)', 
+    category: 'Mixed Formulations',
+    crops: ['Onion', 'Paddy', 'Potato'], 
+    dosage: [
+      'Onion Two soil applications at 10 kg/ha',
+      'Paddy Two soil applications at 10 kg/ha',
+      'Potato Two soil applications at 10 kg/ha'
+    ], 
+    gazette: '13th August, 2025 S.O. 3741(E)', 
+    composition: [
+      '(i) Seaweed extract (Ascophyllum nodosum) per cent. by weight, maximum 0.5', 
+      '(ii) Protein hydrolysate derived from defatted soy flour by enzymatic (papain) hydrolysis per cent. by weight, minimum 0.5',
+      '(iii) Humic acid (source: Leonardite) per cent. by weight, minimum 0.02',
+      '(iv) Polysorbate 80 per cent. by weight, minimum 0.05',
+      '(v) Bentonite granules per cent. by weight QS',
+      '(vi) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'PROTEIN PREMIUM', 
+    specification: 'Mixture of Seaweed extract, Protein Hydrolysate and Humic acid (Powder)', 
+    category: 'Mixed Formulations',
+    crops: ['Okra'], 
+    dosage: ['Two foliar applications at 500 g/ha'], 
+    gazette: '13th August, 2025 S.O. 3741(E)', 
+    composition: [
+      '(i) Seaweed (Ascophyllum nodosum) extract per cent. by weight, minimum 22', 
+      '(ii) Protein hydrolysates derived from gelatin through chemical Hydrolysis with calcium hydroxide and neutralized with sulphuric acid per cent. by weight, minimum 8',
+      '(iii) Humic acid (Source: Leonardite) per cent. by weight, minimum 58',
+      '(iv) Potassium sulphate per cent. by weight, minimum 5',
+      '(v) Calcium hydrogen phosphate (as inert) per cent. by weight, minimum 1',
+      '(vi) Malic acid per cent. by weight 6',
+      '(vii) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'S GOLD', 
+    specification: 'Mixture of Seaweed extract, Protein Hydrolysate and Humic acid (Granule)', 
+    category: 'Mixed Formulations',
+    crops: ['Groundnut'], 
+    dosage: ['Two soil applications at 7.5 kg/ha'], 
+    gazette: '13th August, 2025 S.O. 3741(E)', 
+    composition: [
+      '(i) Seaweed (Ascophyllum nodosum) extract per cent. by weight, minimum 0.83', 
+      '(ii) Protein hydrolysates derived from gelatin through chemical Hydrolysis with calcium hydroxide and neutralized with sulphuric acid per cent. by weight, minimum 0.33',
+      '(iii) Humic acid (Source: Leonardite) per cent. by weight, minimum 1.3',
+      '(iv) Calcium hydrogen phosphate (as inert) per cent. by weight, minimum 1.0',
+      '(v) Dolomite granules (as carrier) per cent. by weight, minimum 96.54',
+      '(vi) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'SIOL PREMIUM', 
+    specification: 'Mixture of Seaweed extract, Humic acid and Fulvic acid (Granules)', 
+    category: 'Mixed Formulations',
+    crops: ['Chilli'], 
+    dosage: ['Three foliar applications at 300 g/ha'], 
+    gazette: '13th August, 2025 S.O. 3742(E)', 
+    composition: [
+      '(i) Seaweed (Ascophyllum nodosum) extract per cent. by weight, minimum 10.00', 
+      '(ii) Humic acid (Source: Leonardite) per cent. by weight, minimum 4.00',
+      '(iii) Fulvic acid per cent. by weight, minimum 2.00',
+      '(iv) Potassium hydroxide per cent. by weight, minimum 2',
+      '(v) Polysorbate 80 per cent. by weight, minimum 5',
+      '(vi) Starch per cent. by weight, minimum 1',
+      '(vii) Lactose per cent. by weight, minimum QS',
+      '(viii) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'VITA SPECIAL', 
+    specification: 'Mixture of Vitamin and Biochemical (Powder)', 
+    category: 'Mixed Formulations',
+    crops: ['Chilli'], 
+    dosage: ['Two foliar applications at 1.5 kg/ha'], 
+    gazette: '16th February, 2026 S.O. 876(E)', 
+    composition: [
+      '(i) Ascorbic acid per cent. by weight, minimum 40', 
+      '(ii) Citric acid per cent. by weight, minimum 10',
+      '(iii) Guar gum per cent. by weight, minimum 15',
+      '(iv) Sodium benzoate per cent. by weight, minimum 10',
+      '(v) Calcium chloride per cent. by weight, minimum 15',
+      '(vi) Calcium carbonate per cent. by weight, maximum 10',
+      '(vii) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'BUMPER', 
+    specification: 'Glutamic acid 5% (Powder)', 
+    category: 'Protein Hydrolysates and Amino Acids',
+    crops: ['Brinjal'], 
+    dosage: ['Three foliar applications at 1000g/ha'], 
+    gazette: '13th August, 2025 S.O. 3741(E)', 
+    composition: [
+      '(i) Glutamic acid per cent. by weight, minimum 5', 
+      '(ii) Dextrin per cent. by weight, maximum 95',
+      '(iii) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'GRAND', 
+    specification: 'Protein Hydrolysate 68.33% (Animal source) (Liquid)', 
+    category: 'Protein Hydrolysates and Amino Acids',
+    crops: ['Rice', 'Tomato'], 
+    dosage: [
+      'Rice Two foliar applications at 1000 ml/ha',
+      'Tomato Two foliar applications at 1000 ml/ha'
+    ], 
+    gazette: '13th August, 2025 S.O. 3741(E)', 
+    composition: [
+      '(i) Protein Hydrolysate derived from Sus domesticus (animal source) through protease hydrolysis per cent. by weight, minimum 68.33', 
+      '(ii) Sodium gluconate 2.94',
+      '(iii) Triethanolamine per cent. by weight, minimum 3.33',
+      '(iv) Citric acid per cent. by weight, minimum 1.73',
+      '(v) Water per cent. by weight QS',
+      '(vi) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'SUPER CHILLY', 
+    specification: 'Glycine 5% (Powder)', 
+    category: 'Protein Hydrolysates and Amino Acids',
+    crops: ['Brinjal'], 
+    dosage: ['Three foliar applications at 1000g/ha'], 
+    gazette: '13th August, 2025 S.O. 3741(E)', 
+    composition: [
+      '(i) Glycine per cent. by weight, minimum 5', 
+      '(ii) Dextrin per cent. by weight, maximum 95',
+      '(iii) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'SUPER COP', 
+    specification: 'Proline 1% (Liquid)', 
+    category: 'Protein Hydrolysates and Amino Acids',
+    crops: ['Capsicum'], 
+    dosage: ['Three foliar applications at 500 ml/ha'], 
+    gazette: '13th August, 2025 S.O. 3741(E)', 
+    composition: [
+      '(i) Proline per cent. by weight, minimum 1', 
+      '(ii) Water per cent. by weight 99',
+      '(iii) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'FLORICA', 
+    specification: 'Microbial cell (Methylococcus):1*109 cfu/g (Powder)', 
+    category: 'Live Microorganisms',
+    crops: ['Paddy', 'Maize'], 
+    dosage: [
+      'Paddy: Three applications (One seedling dip and two foliar applications) at 40 g/ha',
+      'Maize: Two foliar applications at 40 g/ha'
+    ], 
+    gazette: '26th May, 2025 S.O. 2346(E)', 
+    composition: [
+      '(i) Microbial cell (Methylococcus capsulatus) concentrate, cfu/g, minimum 1x109'
+    ] 
+  },
+  { 
+    brand: 'EXACT', 
+    specification: 'Microbial Consortium 1*107 cfu/g', 
+    category: 'Live Microorganisms',
+    crops: ['Onion'], 
+    dosage: ['Single soil application just before planting at 7.5 kg/ha as basal dose'], 
+    gazette: '26th May, 2025 S.O. 2346(E)', 
+    composition: [
+      '(i) Microbial consortium (cfu/ g minimum) per cent. by weight, minimum consisting of 1 x 107',
+      'a. Methylobacterium symbioticum (cfu/g minimum) 0.5 x 107',
+      'b. Methylobacterium extrorquens (cfu/g minimum) 0.5 x 107',
+      '(ii) Carboxymethyl cellulose per cent. by weight, minimum 0.2',
+      '(iii) Bentonite/Talc per cent. by weight, maximum 98.8',
+      '(iv) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'SMASHER', 
+    specification: 'Non Saccharomyces yeast (Wickerhamomyces anomalus) 20 % (Powder)', 
+    category: 'Live Microorganisms',
+    crops: ['Rice'], 
+    dosage: ['Three soil applications at 200g/ha'], 
+    gazette: '13th August, 2025 S.O. 3741(E)', 
+    composition: [
+      '(i) Non Saccharomyces yeast (Wickerhamomyces anomalus strain TSPPTL/MSD-16) 20',
+      '(ii) Dextrose (Carrier) per cent. by weight, minimum 80',
+      '(iii) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'SUPER SEED', 
+    specification: '2-Bromo-(1H)-Indole-3 Carboxaldehyde 1ppm (Liquid)', 
+    category: 'Biochemicals',
+    crops: ['Tomato'], 
+    dosage: ['Seed treatment at 1 ppm formulation'], 
+    gazette: '26th May, 2025 S.O. 2346(E)', 
+    composition: [
+      '(i) 2- Bromo- (l H)- Indole- 3 Carboxaldehyde per cent. by weight, minimum 0.0001',
+      '(ii) Water per cent. by weight QS',
+      '(iii) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'THRIBLE', 
+    specification: 'Homobrassinolide- 0.04% (Liquid)', 
+    category: 'Biochemicals',
+    crops: ['Tea', 'Grape', 'Groundnut', 'Soybean', 'Paddy', 'Cotton', 'Tomato'], 
+    dosage: [
+      'Tea: Three foliar applications at 250 ml/ha',
+      'Grape: Two foliar applications at 1000 ml/ha',
+      'Groundnut: Two foliar applications at 250 ml/ha',
+      'Soybean: Two foliar applications at 250 ml/ha',
+      'Paddy: Two foliar applications at 250 ml/ha',
+      'Cotton: Two foliar applications at 250 ml/ha',
+      'Tomato: Two foliar applications at 250 ml/ha'
+    ], 
+    gazette: '13th August, 2025 S.O. 3741(E)', 
+    composition: [
+      '(i) Homobrassinolide (Source: Synthetic) per cent. by weight, minimum 0.04',
+      '(ii) Polysorbate 20 as adjuvant per cent. by weight, minimum 2.00',
+      '(iii) 4- Nonylphenol ethoxylated per cent. by weight, minimum 2.00',
+      '(iv) Isopropyl alcohol per cent. by weight QS',
+      '(v) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'SUCHANG MAGIK', 
+    specification: 'Vitamin B3 (Nicotinamide) 3.25% (liquid)', 
+    category: 'Vitamins',
+    crops: ['Tomato'], 
+    dosage: ['Two foliar applications at 12.5 ml/ha'], 
+    gazette: '13th August, 2025 S.O. 3741(E)', 
+    composition: [
+      '(i) Vitamin B3 – Niacinamide (Nicotinamide) (synthetic) per cent. by weight, minimum 3.25',
+      '(ii) Emulsifier (Nonyl Phenol Ethoxylate) per cent. by weight, minimum 16.75',
+      '(iii) Solvent (Dimethyl sulfoxide) per cent. by weight, minimum 80',
+      '(iv) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'EXPERT', 
+    specification: 'Vitamin (Alpha Tocopherol) 1% (Liquid)', 
+    category: 'Vitamins',
+    crops: ['Chilli'], 
+    dosage: ['Three foliar applications at 1000 ml/ha'], 
+    gazette: '13th August, 2025 S.O. 3741(E)', 
+    composition: [
+      '(i) Alpha tocopherol (Vitamin-E) (Source: Sunflower Oil) per cent. by weight, minimum 1',
+      '(ii) Tween 80 per cent. by weight, minimum 6',
+      '(iii) Cottonseed oil per cent. by weight, minimum 93',
+      '(iv) Total (per cent.) 100'
+    ] 
+  },
+  { 
+    brand: 'MESMERIZE', 
+    specification: 'Folic acid 0.1% (Liquid)', 
+    category: 'Vitamins',
+    crops: ['Chilli'], 
+    dosage: ['Four foliar applications at 200 ml/ha'], 
+    gazette: '16th February, 2026 S.O. 876(E)', 
+    composition: [
+      '(i) Folic acid per cent. by weight, minimum 0.1',
+      '(ii) Polysorbate 20 per cent. by weight, maximum 3.0',
+      '(iii) Sodium bicarbonate per cent. by weight, maximum 2.5',
+      '(iv) Water per cent. by weight QS',
+      '(v) Total (per cent.) 100'
+    ] 
+  }
+];
+
+export const SLUG_TO_BRAND = PRODUCTS.reduce((acc, product) => {
+  const encoded = encodeURIComponent(product.brand);
+  const hyphenated = product.brand.replace(/ /g, '-');
+  acc[product.brand] = product.brand;
+  acc[hyphenated] = product.brand;
+  acc[encoded] = product.brand;
+  return acc;
+}, {});
+
+export function findProductBySlug(slug) {
+  if (!slug) return PRODUCTS[0];
+  
+  const decoded = decodeURIComponent(slug).toUpperCase();
+  const normalized = decoded.replace(/-/g, ' ').replace(/\+/g, ' ').trim();
+  
+  // Try direct match in SLUG_TO_BRAND
+  if (SLUG_TO_BRAND[normalized]) return findProductByBrand(SLUG_TO_BRAND[normalized]);
+  if (SLUG_TO_BRAND[decoded]) return findProductByBrand(SLUG_TO_BRAND[decoded]);
+
+  // Try matching against brand names directly
+  const found = PRODUCTS.find(p => 
+    p.brand.toUpperCase() === normalized || 
+    p.brand.toUpperCase().replace(/\s+/g, '') === normalized.replace(/\s+/g, '')
+  );
+  
+  return found || PRODUCTS[0];
+}
+
+export function findProductByBrand(brand) {
+  return PRODUCTS.find(p => p.brand.toUpperCase() === brand.toUpperCase()) || PRODUCTS[0];
+}
